a9fb22334109f61e8d3b9a5ea91dc6e41ba09916
rolandobrown <rolando.brown@gmail.com>
2015-08-02 19:27:21 -0400

    loop Wingardium Leviosa 7 times

--- times.rb
+++ times.rb
@@ -1,7 +1,19 @@
 def using_times
-	#your code here
+	7.times do 
+    puts "Wingardium Leviosa"
+  end
 end
 
+## DONE
 
+# 1. Fork and clone this lab.
+# 2. Run the test suite with the `learn` command. 
+# 3. You'll be coding your solution in `times.rb`: 
+# 4. Fill out the content of the `using_times` method so that 
+# calling it will `puts` the desired phrase, `"Wingardium Leviosa"` 
+# seven times by using the `times` keyword.  
 
+## PENDING
+
+# all done
 
